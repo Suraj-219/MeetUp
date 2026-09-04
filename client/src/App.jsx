dacc1fe4c7fab1610921f9b52269b01bfe9df4d4
Suraj Pal <surajpal7442@gmail.com>
2026-09-04 15:12:34 +0530

    Add routing for protected and public routes, and implement Navbar, Footer, and Loader components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-import { Routes } from 'react-router-dom'
+import { Navigate, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Sessions from './pages/Sessions.jsx'
+import Pricing from './pages/Pricing.jsx'
+import MeetingRoom from './pages/MeetingRoom.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+import Protectedlayout from './components/Protectedlayout.jsx'
 
 
 const App = () => {
@@ -11,13 +16,22 @@ const App = () => {
       <Toaster />
       <Routes>
         {/* Public routes */}
-        <Route path='/login' elements={ <Login mode="login"/> } />
-        <Route path='/register' elements={ <Login mode="register"/> } />
+        <Route path='/login' element={ <Login mode="login"/> } />
+        <Route path='/register' element={ <Login mode="register"/> } />
 
 
         {/* Private routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route element={<Protectedlayout />}>
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/sessions' element={<Sessions />} />
+            <Route path='/pricing' element={<Pricing />} />
+          </Route>
+          <Route path='/meeting/:meetingId' element={<MeetingRoom />} />
+        </Route>
 
         {/* Other routes */}
+        <Route path='*' element={<Navigate to='/dashboard' replace />} />
 
       </Routes>
     </>
