238ec45f8937df531ec32eb0f0df4c7a2520bc34
Suraj Pal <surajpal7442@gmail.com>
2026-09-04 14:51:42 +0530

    Implement routing with public routes and add ProtectedRoute component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Routes } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login.jsx'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Toaster />
+      <Routes>
+        {/* Public routes */}
+        <Route path='/login' elements={ <Login mode="login"/> } />
+        <Route path='/register' elements={ <Login mode="register"/> } />
+
+
+        {/* Private routes */}
+
+        {/* Other routes */}
+
+      </Routes>
+    </>
   )
 }
 
